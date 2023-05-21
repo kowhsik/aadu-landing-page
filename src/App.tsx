fb0b497ce473b208a33f99f02116e232e9b57b9b
suganya05 <101466647+suganya05@users.noreply.github.com>
2023-05-21 17:56:45 +0530

    chore: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,62 @@
 import React from "react";
-import Image1 from "./asserts/images/Frame 2 1.svg";
-import Image2 from "./asserts/images/Coming Soon.svg";
+import Image1 from "./asserts/images/Middle.png";
 import logo from "./asserts/images/logo.svg";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import Image3 from "./asserts/images/WeGotYou.png";
 import Image4 from "./asserts/images/OurClients.png";
+import Image5 from "./asserts/images/Middle-media.png";
 
 import "./App.css";
 
 const settings = {
-  dots: true,
+  dots: false,
   infinite: true,
   speed: 500,
   slidesToShow: 1,
   slidesToScroll: 1,
+  autoplay: true,
 };
 
-function App() {
+const App: React.FC = () => {
   return (
     <>
       <div className="slider-container">
         <Slider {...settings}>
+          <div className="image-container">
+            <div>
+              <img
+                src={Image1}
+                alt=""
+                className="a-img"
+                style={{ margin: "94px 40px" }}
+              />
+            </div>
+            <div className="media-img">
+              <img src={Image5} alt="" />
+            </div>
+          </div>
+
           <div>
-            <img src={Image1} alt="" className="a-img" />
-            <img src={Image2} alt="" className="comin-soon-img" />
+            <img src={Image3} alt="" className="weGoYou" />
           </div>
           <div>
-            <img src={Image3} alt="" className="we go tou" />
-          </div>
-          <div>
-            <img src={Image4} alt="" className="our Clients" />
+            <img src={Image4} alt="" className="ourClients" />
           </div>
         </Slider>
       </div>
-      {/* <div className="App">
-        <div className="wrapper">
-          <div className="wrapper-one">
-            <img src={Image1} alt="" className="a-img" />
-            <img src={Image2} alt="" className="comin-soon-img" />
-          </div>
-          <div className="wrapper-two">
-            <img src={logo} alt="" />
-            <div className="content">
-              <h4>hello@aadu.agency</h4>
-              <h4>6/170, Thotiyankadu, 639136 IN</h4>
-            </div>
-          </div>
+      <div className="wrapper-two">
+        <div className="logo">
+          <img src={logo} alt="" />
         </div>
-      </div> */}
+        <div className="content">
+          <h4>hello@aadu.agency</h4>
+          <h5>6/170, Thotiyankadu, 639136 IN</h5>
+        </div>
+      </div>
     </>
   );
-}
+};
 
 export default App;
