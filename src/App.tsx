2c77ba7a5b19b196a01c91ed24b51d8b4e9fc517
Arunram2000 <70103361+Arunram2000@users.noreply.github.com>
2023-08-13 20:24:02 +0530

    feat: audio tag added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React from "react";
-import Image1 from "./asserts/images/Middle.png";
-import logo from "./asserts/images/logo.svg";
+import React, { useEffect, useRef, useState } from "react";
+import Slider from "react-slick";
+
+import "./App.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+
+import Image1 from "./asserts/images/Middle.png";
+import logo from "./asserts/images/logo.svg";
 import Image3 from "./asserts/images/We've got you..png";
 import Image4 from "./asserts/images/Our Clients (1).png";
 import Image5 from "./asserts/images/Middle-media.png";
 import Image6 from "./asserts/images/wegotyou2.png";
 import Image7 from "./asserts/images/our-clients.png";
-import { useEffect } from "react";
-
-import "./App.css";
+import { ReactComponent as VolumeIcon } from "./asserts/icons/volume-2.svg";
+import { ReactComponent as MuteIcon } from "./asserts/icons/volume-x.svg";
 
 const settings = {
   dots: false,
@@ -27,6 +29,19 @@ const settings = {
 };
 
 const App: React.FC = () => {
+  const [isMuted, setIsMuted] = useState(true);
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      if (!isMuted) {
+        audioRef.current.play();
+      } else {
+        audioRef.current.pause();
+      }
+    }
+  }, [isMuted]);
+
   useEffect(() => {
     const refreshPage = () => {
       window.location.reload();
@@ -45,12 +60,7 @@ const App: React.FC = () => {
           <Slider {...settings}>
             <div className="image-container">
               <div>
-                <img
-                  src={Image1}
-                  alt=""
-                  className="a-img"
-                  style={{ margin: "94px 40px" }}
-                />
+                <img src={Image1} alt="" className="a-img" style={{ margin: "94px 40px" }} />
               </div>
               <div className="media-img">
                 <img src={Image5} alt="" className="media-img" />
@@ -92,6 +102,16 @@ const App: React.FC = () => {
           </h4>
           <h5 style={{ color: "#F0F0F0" }}>6/170, Thotiyankadu, 639136 IN</h5>
         </div>
+      </div>
+
+      <div className="audio">
+        <audio ref={audioRef} autoPlay loop style={{ display: "none" }}>
+          <source src={"/background.mp3"} type="audio/mp3" />
+          Your browser does not support the audio element.
+        </audio>
+        <button onClick={() => setIsMuted(!isMuted)}>
+          {isMuted ? <VolumeIcon /> : <MuteIcon />}
+        </button>
       </div>
     </>
   );
